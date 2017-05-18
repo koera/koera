a5e23b22fe586cab049c2afe5cf172ad473e4878
koera-sam <koera-sam@koera-sam>
2017-05-18 21:29:31 +0300

    Message 2

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -7,12 +7,15 @@ include_once '../models/Users.php';
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'connect') {
-        if (isset($_GET['login']) and isset($_GET['password'])) {
-            $users = Users::getUsers($pdo, $_GET['login'], $_GET['password']);
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == 'connect') {
+        if (isset($_POST['login']) and isset($_POST['password'])) {
+            $users = Users::getUsers($pdo, $_POST['login'], $_POST['password']);
             if ($users != null) {
-                $_SESSION['username'] = $users->getLogin();
+                session_start();
+                $_SESSION["username"] = $users->getLogin();
+                //print_r($_SESSION);
+                print 'session_begin';
             } else {
                 print 'Login ou mot de passe non trouve';
             }
