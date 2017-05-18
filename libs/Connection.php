ef332dc2527894563353e1e71c7e864928772ce3
koera-sam <koera-sam@koera-sam>
2017-05-18 16:26:56 +0300

    Changed

--- libs/Connection.php
+++ libs/Connection.php
@@ -1,11 +1,10 @@
 <?php
 
 /*
-
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-
+ * ab
  */
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=db_trigger', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
